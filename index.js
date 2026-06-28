8325caf349a60e7de36f8b40dcda953eabfb989e
umayer10-AI <mdumayer10@gmail.com>
2026-06-28 14:22:25 +0600

    add

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ mongoose.connect('mongodb://127.0.0.1:27017/testdb')
 const run = async() => {
     try{
         
-        const user = await User.create({
-            name: "umayer",
-            age: 21,
-            email: "umayer@gmail.com",
-            address: {
-                street: "Fatullah",
-                city: "Dhaka"
-            }
-        })
+        // const user = await User.create({
+        //     name: "ahmad",
+        //     age: 21,
+        //     email: "umayer@gmail.com",
+        //     address: {
+        //         street: "Fatullah",
+        //         city: "Dhaka"
+        //     }
+        // })
 
         // const user = new User({
         //     name: "umayer",
@@ -23,7 +23,14 @@ const run = async() => {
         // })
         // await user.save()
 
-        console.log("Save")
+        // const user = await User.findById('6a40d6adadfa2d1f09d88955')
+        // const user = await User.find({name: 'umayer'})
+        // const user = await User.exists({name: 'umayer'})
+        // const user = await User.deleteOne({name: 'umayer'})
+        // const user = await User.where('age').gt(20).lt(35).where('name').equals('ahmad')
+
+        console.log(user)
+        console.log(user.length)
     }
     catch(e){
         console.log(e.message)
